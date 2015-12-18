12747fdc28d0d653cb2e8b20f6c4249a56a39758
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2015-12-18 10:42:39 +0100

    Adding a model that actually compiles

--- inc/model_defs.h
+++ inc/model_defs.h
@@ -1,15 +1,15 @@
-#ifndef MULTICORE_TOPO_DEFS
-#define MULTICORE_TOPO_DEFS 1
+#ifndef MULTICORE_MODEL_DEFS
+#define MULTICORE_MODEL_DEFS 1
 
-#define MACHINE "<class 'gruyere.Gruyere'>"
+#define MACHINE "<class 'netos_machine.NetosMachine'>"
 #define TOPO_NUM_CORES 32
 
-#define TOPOLOGY "multi-topo"
+#define TOPOLOGY "multi-model"
 #define SHM_SLAVE_START 50
 #define SHM_SLAVE_MAX 69
 #define SHM_MASTER_START 70
 #define SHM_MASTER_MAX 89
-#define NUM_TOPOS 5
-#define ALL_LAST_NODES ((int[]) {31, 28, 24, 23, 29})
+#define NUM_TOPOS 1
+#define ALL_LAST_NODES ((int[]) {31})
 #define LAST_NODE ALL_LAST_NODES[get_topo_idx()]
 #endif
