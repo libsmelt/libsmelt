0d5765d3ef769eca786203e2a867147aa74dd712
Roni Häcki <roni.haecki@inf.ethz.ch>
2015-12-23 16:54:19 +0100

    Added model

---
+++ inc/models/820/model_defs.h
@@ -0,0 +1,15 @@
+#ifndef MULTICORE_MODEL_DEFS
+#define MULTICORE_MODEL_DEFS 1
+
+#define MACHINE "<class 'netos_machine.NetosMachine'>"
+#define TOPO_NUM_CORES 32
+
+#define TOPOLOGY "multi-model"
+#define SHM_SLAVE_START 50
+#define SHM_SLAVE_MAX 69
+#define SHM_MASTER_START 70
+#define SHM_MASTER_MAX 89
+#define NUM_TOPOS 6
+#define ALL_LAST_NODES ((int[]) {23, 15, 29, 5, 24, 9})
+#define LAST_NODE ALL_LAST_NODES[get_topo_idx()]
+#endif
