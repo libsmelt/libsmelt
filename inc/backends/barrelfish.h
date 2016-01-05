5ba2f100847fcde39ee32cc39ee61fdf5cf3fb40
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-01-05 14:05:52 +0100

    barrelfish backend: adding code to backedn files
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- inc/backends/barrelfish.h
+++ inc/backends/barrelfish.h
@@ -1,8 +1,27 @@
 #include <barrelfish_kpi/types.h>
+
+//#include <barrelfish/waitset.h>
+//#include <barrelfish/ump_impl.h>
+
+#include <bench/bench.h>
+
+//#include <if/sync_defs.h>
+//#include <barrelfish/nameservice_client.h>
+
+#ifdef FFQ
+
+#include "ff_queue.h"
+#include "ffq_conf.h"
+
+typedef struct ffq_pair_state mp_binding;
+
+#else // UMP
+#include "ump_conf.h"
+#include "ump_common.h"
+
+typedef struct ump_pair_state mp_binding;
+
+#define UMP_QUEUE_SIZE 1000U
+
 #include <barrelfish/barrelfish.h>
-#include <barrelfish/waitset.h>
-#include <barrelfish/ump_impl.h>
-
-#include <if/sync_defs.h>
-#include <barrelfish/nameservice_client.h>
-
+#endif
