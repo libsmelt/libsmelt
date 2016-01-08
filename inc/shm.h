c8a48b97121549787c27c4eee8773058e0d138a7
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-01-08 14:59:26 +0100

    shm: defined interface + initial copy of SHM protocol

--- inc/shm.h
+++ inc/shm.h
@@ -14,4 +14,35 @@ int map_master_share(void);
 
 union quorum_share* get_master_share(void);
 
+
+void shm_send(struct libsync_context* context,
+              uintptr_t p1,
+              uintptr_t p2,
+              uintptr_t p3,
+              uintptr_t p4,
+              uintptr_t p5,
+              uintptr_t p6,
+              uintptr_t p7);
+
+/*
+ * Receiver
+ *
+ * void bla(uintptr_t d1, ... ) {
+ * uintptr_t d1, d2, d3 .. 
+ * uintptr_t d[7];
+ * shm_receive(..., &d1, &d2, .. )
+ * sth(d);
+ * }
+ * 
+ */
+
+void shm_receive(struct libsync_context* context,
+              uintptr_t *p1,
+              uintptr_t *p2,
+              uintptr_t *p3,
+              uintptr_t *p4,
+              uintptr_t *p5,
+              uintptr_t *p6,
+              uintptr_t *p7);
+
 #endif /* SYNC_SHM_H */
