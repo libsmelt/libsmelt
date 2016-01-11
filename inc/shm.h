93d4ea747703e91809ceb41a02d3a9d5ca2fa2d8
Roni Häcki <roni.haecki@inf.ethz.ch>
2016-01-11 10:03:46 +0100

    SHM queue integrated and compiling

--- inc/shm.h
+++ inc/shm.h
@@ -9,13 +9,62 @@
 #ifndef SYNC_SHM_H
 #define SYNC_SHM_H 1
 
+#include <inttypes.h>
+
 int init_master_share(void);
 int map_master_share(void);
 
 union quorum_share* get_master_share(void);
 
+/*
+ * Definitions for shared memory queue
+ */
 
-void shm_send(struct libsync_context* context,
+#define SHMQ_SIZE 4096
+#define CACHELINE_SIZE 64
+
+union __attribute__((aligned(64))) pos_pointer{
+    uint64_t pos;
+    uint8_t padding[CACHELINE_SIZE];
+};
+
+struct shm_queue{	
+    // The shared memory itself
+    uint8_t* shm;
+    uint8_t* data;
+    // Positions of readers/writer within shared queue
+    union pos_pointer* write_pos;
+    union pos_pointer* readers_pos;
+    // Number of readers
+    uint8_t num_readers;
+    // Reader id, only unique within cluster
+    uint8_t id;
+    // Number of slots (depends on num_readers)
+    uint16_t num_slots;
+    /* local position pointer, global will be updated
+     * when the end of the queue is reached
+     */
+    uint16_t l_pos;
+
+};
+
+/*
+ * \brief Initializ a shared memory context
+ *
+ * The memory must already be shared between the 
+ * cluster cores
+ * 
+ * \param shm           the shared memory itself
+ * \param num_readers   the number of readers  
+ *
+ * \return the initialized per thread struct
+ */
+
+struct shm_queue* shm_init_context(void* shm,
+                                   uint8_t num_readers);
+
+
+void shm_send(struct shm_queue* context,
               uintptr_t p1,
               uintptr_t p2,
               uintptr_t p3,
@@ -36,7 +85,7 @@ void shm_send(struct libsync_context* context,
  * 
  */
 
-void shm_receive(struct libsync_context* context,
+void shm_receive(struct shm_queue* context,
               uintptr_t *p1,
               uintptr_t *p2,
               uintptr_t *p3,
@@ -44,5 +93,4 @@ void shm_receive(struct libsync_context* context,
               uintptr_t *p5,
               uintptr_t *p6,
               uintptr_t *p7);
-
 #endif /* SYNC_SHM_H */
