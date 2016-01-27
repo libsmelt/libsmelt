7082329d40f2dd4dc35c52d8296394f95cba4b09
Roni Häcki <roni.haecki@inf.ethz.ch>
2016-01-27 19:08:01 +0100

    Babybel model

---
+++ inc/models/babybel1/model_defs.h
@@ -0,0 +1,13 @@
+#ifndef MULTICORE_MODEL_DEFS
+#define MULTICORE_MODEL_DEFS 1
+
+#define MACHINE "<class 'netos_machine.NetosMachine'>"
+#define TOPO_NUM_CORES 20
+
+#define TOPOLOGY "multi-model"
+#define SHM_SLAVE_START 50
+#define SHM_SLAVE_MAX 69
+#define SHM_MASTER_START 70
+#define SHM_MASTER_MAX 89
+#define NUM_TOPOS 6
+#endif
