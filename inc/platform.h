92e7653a10e7657d7093420e5d7848372476584e
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-02-04 16:40:35 +0100

    T196 - adding header comment to platforms.h
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- inc/platform.h
+++ inc/platform.h
@@ -1,5 +1,10 @@
-/**
- * 
+/*
+ * Copyright (c) 2016 ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
  */
 #ifndef SMLT_PLATFORM_H_
 #define SMLT_PLATFORM_H_ 1
