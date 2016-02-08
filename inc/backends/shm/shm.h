737c9b95da085e9ee33f55a69d1e2424ee051532
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-02-08 13:50:06 +0100

    T200 - moving layout of shared page to shm backend.
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- inc/backends/shm/shm.h
+++ inc/backends/shm/shm.h
@@ -15,6 +15,65 @@
 int init_master_share(void);
 int map_master_share(void);
 
+// Layout of the shared page
+union quorum_share {
+    // Structure
+    struct {
+        uint32_t __attribute__ ((aligned(32))) dummy; // < test mapping
+        uint32_t __attribute__ ((aligned(32))) ctr; // cnt clients
+#if defined(FEEDBACK)
+        // Feedback: for signalling end of experiment
+        uint32_t __attribute__ ((aligned(32))) feedback;
+#endif
+        uint32_t __attribute__ ((aligned(32))) cores[Q_MAX_CORES]; // core state
+
+        // --- 16 bytes
+
+        // Several counters on the same cache-line, but we don't care
+        // as different counters are never used concurrently anyway
+        spinlock_t __attribute__ ((aligned(64))) lock;
+
+        // --  8 bytes
+
+        uint8_t __attribute__ ((aligned(32))) rounds[QRM_ROUND_MAX];
+
+        // -- 4 bytes * 500
+
+        // -- SUM: 2024
+
+        // FIFO - message channels: these are used in shared-memory
+        // communication channels to transport messages. It's
+        // essentially a one-writer multiple-reader queue.
+
+        // Currently shared by all cores participating in a
+        // shared-memory island.
+
+        // There used to be four queues here, but only the first one
+        // was ever used. I am going to remove the remaining one to
+        // save some space.
+
+        // shared memory for reduction
+        uint64_t __attribute__((aligned(64))) reduction_aggregate;
+
+        // shared memory for reduction (counter to indicate how many finished)
+        uint64_t __attribute__((aligned(64))) reduction_counter;
+
+        // shared memory for reduction (counter to indicate round)
+        uint8_t __attribute__((aligned(64))) reduction_round; // XXX has to be uint8
+
+        // Count how often this frame has been mapped. Keep this last
+        // to ensure that whole frame is mapped when incrementing
+        // counter after mapping.
+        uint64_t __attribute__((aligned(64))) num_maps;
+
+        /**
+         * \brief Internal barrier used within libsync.
+         */
+        pthread_barrier_t sync_barrier;
+    } data;
+};
+
+
 union quorum_share* get_master_share(void);
 
 /*
