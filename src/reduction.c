590ad2f444b39ff9212342e4820fdf8a79800d41
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-02-09 14:27:16 +0100

    T196 - reduction: integration of sync_reduce into smlt_reduce
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- src/reduction.c
+++ src/reduction.c
@@ -21,6 +21,12 @@ errval_t smlt_reduce(struct smlt_msg *input,
         return smlt_reduce_notify();
     }
 
+    // --------------------------------------------------
+    // Shared memory first
+
+    // Note: tree is processed in backwards direction, i.e. a receive
+    // corresponds to a send and vica-versa.
+
     if (smlt_current_instance()->has_shm) {
         smlt_shm_reduce(input, result);
         if (smlt_err_fail(err)) {
@@ -28,6 +34,15 @@ errval_t smlt_reduce(struct smlt_msg *input,
         }
     }
     
+    // --------------------------------------------------
+    // Message passing
+
+    /*
+     * Each client receives (potentially from several children) and
+     * sends only ONCE. On which level of the tree hierarchy this is
+     * does not matter. If a client would send several messages, we
+     * would have circles in the tree.
+     */
     struct smlt_node *p = smlt_node_get_parent();
     if (smlt_node_is_leaf()) {    
         smlt_node_send(p, input);
@@ -59,12 +74,28 @@ errval_t smlt_reduce_notify(void)
 {
     errval_t err;
 
+    // --------------------------------------------------
+    // Shared memory first
+
+    // Note: tree is processed in backwards direction, i.e. a receive
+    // corresponds to a send and vica-versa.
+
     if (smlt_current_instance()->has_shm) {
         err = shm_reduce_notify();
         if (smlt_err_fail(err)) {
             return err;
         }
     }
+
+    // --------------------------------------------------
+    // Message passing
+
+    /*
+     * Each client receives (potentially from several children) and
+     * sends only ONCE. On which level of the tree hierarchy this is
+     * does not matter. If a client would send several messages, we
+     * would have circles in the tree.
+     */
 
     struct smlt_node *p = smlt_node_get_parent();
     if (smlt_node_is_leaf()) {    
@@ -106,61 +137,6 @@ errval_t smlt_reduce_all(struct smlt_msg *input,
     }
 
     return smlt_broadcast(result);
-}
-
-
-
-/**
- * \brief
- *
- */
-uintptr_t sync_reduce(uintptr_t val)
-{
-    // --------------------------------------------------
-    // Shared memory first
-
-    // Note: tree is processed in backwards direction, i.e. a receive
-    // corresponds to a send and vica-versa.
-
-    uintptr_t current_aggregate = val;
-
-    current_aggregate += shm_reduce(current_aggregate);
-
-    // --------------------------------------------------
-    // Message passing
-
-    /*
-     * Each client receives (potentially from several children) and
-     * sends only ONCE. On which level of the tree hierarchy this is
-     * does not matter. If a client would send several messages, we
-     * would have circles in the tree.
-     */
-    return mp_reduce(current_aggregate);
-}
-
-uintptr_t sync_reduce0(uintptr_t val)
-{
-    // --------------------------------------------------
-    // Shared memory first
-
-    // Note: tree is processed in backwards direction, i.e. a receive
-    // corresponds to a send and vica-versa.
-
-    uintptr_t current_aggregate = val;
-
-    current_aggregate += shm_reduce(current_aggregate);
-
-    // --------------------------------------------------
-    // Message passing
-
-    /*
-     * Each client receives (potentially from several children) and
-     * sends only ONCE. On which level of the tree hierarchy this is
-     * does not matter. If a client would send several messages, we
-     * would have circles in the tree.
-     */
-    mp_reduce0();
-    return 0;
 }
 
 
