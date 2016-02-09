459ec4e7a5ad169968db30dabec940c51e912439
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-02-09 10:48:28 +0100

    T196 - removing ab_forward0 and ab_forward7 to obtain clarity
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- src/broadcast.c
+++ src/broadcast.c
@@ -45,8 +45,7 @@ errval_t smlt_broadcast_subtree_notify(void)
 {
     errval_t err;
     uint32_t count;
-    struct smlt_node **nl = smlt_node_get_children(smlt_node_get_self(),
-                                                  &count);
+    struct smlt_node **nl = smlt_node_get_children(&count);
 
     for (uint32_t i = 0; i < count; ++i) {
         err = smlt_node_notify(nl[i]);
@@ -69,7 +68,7 @@ errval_t smlt_broadcast(struct smlt_msg *msg)
     errval_t err;
 
     /* send to parent */
-    struct smlt_node *p = smlt_node_get_parent(smlt_node_get_self());
+    struct smlt_node *p = smlt_node_get_parent();
     if (p) {
         err = smlt_node_send(p, msg);
         // TODO: error handling
@@ -89,7 +88,7 @@ errval_t smlt_broadcast_notify(void)
     errval_t err;
 
     /* send to parent */
-    struct smlt_node *p = smlt_node_get_parent(smlt_node_get_self());
+    struct smlt_node *p = smlt_node_get_parent();
     if (p) {
         err = smlt_node_notify(p);
         // TODO: error handling
@@ -134,71 +133,6 @@ uintptr_t ab_forward(uintptr_t val, coreid_t sender)
             mp_send_ab(val);
         } else {
             val = mp_receive_forward(0);
-        }
-    }
-
-    // Shared memory
-    // --------------------------------------------------
-    if (topo_does_shm_send(my_core_id) || topo_does_shm_receive(my_core_id)) {
-
-        if (topo_does_shm_send(my_core_id)) {
-
-            // send
-            // --------------------------------------------------
-
-            debug_printfff(DBG__HYBRID_AC, "Starting SHM .. -- send, \n");
-
-            shm_send(val, 0, 0, 0, 0, 0, 0);
-        }
-        if (topo_does_shm_receive(my_core_id)) {
-
-            // receive
-            // --------------------------------------------------
-
-            debug_printfff(DBG__HYBRID_AC, "Starting SHM .. -- receive, \n");
-
-            uintptr_t val1, val2, val3, val4, val5, val6, val7;
-            shm_receive(&val1, &val2, &val3, &val4, &val5, &val6, &val7);
-        }
-
-        debug_printfff(DBG__HYBRID_AC, "End SHM\n");
-    }
-
-    return val;
-
-}
-
-uintptr_t ab_forward0(uintptr_t val, coreid_t sender)
-{
-    debug_printfff(DBG__HYBRID_AC, "hybrid_ac entered\n");
-
-    coreid_t my_core_id = get_thread_id();
-    bool does_mp = topo_does_mp_receive(my_core_id, false) ||
-        (topo_does_mp_send(my_core_id, false));
-
-    // Sender forwards message to the root
-    if (get_thread_id()==sender && sender!=get_sequentializer()) {
-
-        mp_send(get_sequentializer(), val);
-    }
-
-    // Message passing
-    // --------------------------------------------------
-    if (does_mp) {
-
-        debug_printfff(DBG__HYBRID_AC, "Starting message passing .. "
-                       "does_recv=%d does_send=%d - val=%d\n",
-                       topo_does_mp_receive(my_core_id, false),
-                       topo_does_mp_send(my_core_id, false),
-                       val);
-
-        if (my_core_id == SEQUENTIALIZER) {
-            if (sender!=get_sequentializer()) {
-                val = mp_receive(sender);
-            }
-            mp_send_ab0();
-        } else {
-            mp_receive_forward0();
         }
     }
 
