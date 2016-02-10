d1de8ebf3c4a50316bdaca5d175b76c56f61f1a8
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-02-10 14:04:29 +0100

    T196 - debug: reorganizing debug output functionality
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- src/debug.c
+++ src/debug.c
@@ -1,16 +1,38 @@
-#include <cstdlib>
-#include <cstdio>
-#include <cstdarg>
+/*
+ * Copyright (c) 2016 ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
+ */
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <smlt.h>
 #include "debug.h"
 
-void printw(const char *fmt, ...)
+/**
+ * @brief prints an message to stdout
+ *
+ * @param fmt   format string to print
+ */
+void smlt_debug_print(uint32_t subs, const char *fmt, ...)
 {
     va_list argptr;
     char str[1024];
-    size_t len;
+    size_t len = 0;
 
-    len = snprintf(str, sizeof(str), "\033[1;31mWarning:\033[0m ");
+    if (subs & SMLT_DBG_ERR) {
+        len = snprintf(str, sizeof(str), "\033[1;31mERROR:\033[0m ");
+    } else if (subs & SMLT_DBG_WARN) {
+        len = snprintf(str, sizeof(str), "\033[1;33mWARNING:\033[0m ");
+    } else if (subs & SMLT_DBG_NOTICE) {
+        len = snprintf(str, sizeof(str), "\033[1;36mNOTICE:\033[0m ");
+    }
+
+    /* TODO: include submodule */
+
     if (len < sizeof(str)) {
         va_start(argptr, fmt);
         vsnprintf(str + len, sizeof(str) - len, fmt, argptr);
--- src/debug.h
+++ src/debug.h
@@ -1,60 +1,68 @@
 /*
- * Copyright (c) 2015 ETH Zurich.
+ * Copyright (c) 2016 ETH Zurich.
  * All rights reserved.
  *
  * This file is distributed under the terms in the attached LICENSE file.
  * If you do not find this file, copies can be found by writing to:
  * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
  */
-#ifndef SYNC_DEBUG_H
-#define SYNC_DEBUG_H 1
-// --------------------------------------------------
-// DEBUG
+#ifndef SMLT_DEBUG_H_
+#define SMLT_DEBUG_H_ 1
+
+#define SMLT_DEBUG_ENABLED 1
+
+#define SMLT_DBG_ERR       (1 << 31)
+#define SMLT_DBG_WARN      (1 << 30)
+#define SMLT_DBG_NOTICE    (1 << 29)
 
 /**
  * \brief Selective debug output according to subsystem.
  */
-#define DBG__NONE          (0)
-#define DBG__GENERAL       (1<<0)
-#define DBG__HYBRID_AC     (1<<1)
-#define DBG__QRM_BARRIER   (1<<2)
-#define DBG__SHM           (1<<3)
-#define DBG__AB            (1<<4)
-#define DBG__SWITCH_TOPO   (1<<5)
-#define DBG__REDUCE        (1<<6)
-#define DBG__INIT          (1<<7)
-#define DBG__BINDING       (1<<8)
-#define DBG__UMP           (1<<9)
-#define DBG__FFQ          (1<<10)
-
-#define DBG__BARRIER      (DBG__AB | DBG__REDUCE)
+#define SMLT_DBG__NONE          (0)
+#define SMLT_DBG__GENERAL       (1<<0)
+#define SMLT_DBG__HYBRID_AC     (1<<1)
+#define SMLT_DBG__QRM_BARRIER   (1<<2)
+#define SMLT_DBG__SHM           (1<<3)
+#define SMLT_DBG__AB            (1<<4)
+#define SMLT_DBG__SWITCH_TOPO   (1<<5)
+#define SMLT_DBG__REDUCE        (1<<6)
+#define SMLT_DBG__INIT          (1<<7)
+#define SMLT_DBG__BINDING       (1<<8)
+#define SMLT_DBG__UMP           (1<<9)
+#define SMLT_DBG__FFQ          (1<<10)
+#define SMLT_DBG__ALL          ((1 << 11) -1)
+#define SMLT_DBG__BARRIER      (DBG__AB | DBG__REDUCE)
 
 // Mask for selectively enabling debug output
-#define qrm_debug_mask (DBG__GENERAL | DBG__INIT)
-//#define QRM_DBG_ENABLED
+#define smlt_debug_mask (SMLT_DBG__ALL)
 
 // Debug UMP interconnect
 //#define UMP_DBG_COUNT 1
 
-#ifdef QRM_DBG_ENABLED  /* ------------------------------*/
-#define QDBG(...)              qrm_debug(DBG__GENERAL, __VA_ARGS__)
-#define debug_printff(...)     qrm_debug(DBG__GENERAL, __VA_ARGS__)
-#define debug_printfff(x, ...) qrm_debug(x,            __VA_ARGS__)
-#define dbg_assert(x) assert(x)
-#else /* QRM_DBG_ENABLED --------------------------------*/
-#define QDBG(...) void()
-#define debug_printff(...) void()
-#define debug_printfff(...) void()
-#define dbg_assert(...) void()
-#endif /* QRM_DBG_ENABLED -------------------------------*/
 
-#define qrm_debug(_subs, _fmt, ...) \
+#ifdef SMLT_DEBUG_ENABLED
+#define SMLT_DEBUG(_subs, ... )                                    \
     do {                                                                \
-        if (((_subs) & qrm_debug_mask) )                                \
-            debug_printf(_fmt, ## __VA_ARGS__);                         \
-    } while(0)
+        if (((_subs) & smlt_debug_mask))                                \
+            smlt_debug_print(_subs,  __VA_ARGS__);              \
+    } while(0);
+#else
+#define SMLT_DEBUG(_subs, _fmt, ...) void()
+#endif
 
 
-void printw(const char *fmt, ...);
+/*
+ * ===========================================================================
+ * print functionality
+ * ===========================================================================
+ */
 
-#endif /* SYNC_DEBUG_H */
+/**
+ * @brief prints an message to stdout
+ *
+ * @param fmt   format string to print
+ */
+void smlt_debug_print(uint32_t subs, const char *fmt, ...);
+
+
+#endif /* SMLT_DEBUG_H_ */
