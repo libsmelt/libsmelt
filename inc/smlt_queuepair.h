670685de06e6ca5fe177480d6c333c6a40d1e1a0
Roni Häcki <roni.haecki@inf.ethz.ch>
2016-03-11 09:23:45 +0100

    T200 removed swmr from qp
    Signed-off-by: Roni Häcki <roni.haecki@inf.ethz.ch>

--- inc/smlt_queuepair.h
+++ inc/smlt_queuepair.h
@@ -12,7 +12,6 @@
 #include <ump/smlt_ump_queuepair.h>
 #include <ffq/ff_queuepair.h>
 #include <shm/shm_qp.h>
-#include <shm/swmr.h>
 /*
  * ===========================================================================
  * Smelt queue pair MACROS
@@ -40,7 +39,6 @@ typedef enum {
     SMLT_QP_TYPE_UMP,           ///< qp uses the UMP backend
     SMLT_QP_TYPE_FFQ,           ///< qp uses the FFQ backend
     SMLT_QP_TYPE_SHM,            ///< qp uses shared memory backend
-    SMLT_QP_TYPE_SWMR            ///< qp uses shared memory backend
 } smlt_qp_type_t;
 
 
@@ -87,14 +85,12 @@ struct smlt_qp
         struct smlt_ump_queuepair ump;
         struct ff_queuepair ffq;
         struct shm_qp shm;
-        struct swmr_queue swmr;
     } queue_tx;
 
     union {
         struct smlt_ump_queuepair ump;
         struct ff_queuepair ffq;
         struct shm_qp shm;
-        struct swmr_queue swmr;
     } queue_rx;
 
     struct {
