094e59cfc2ce0964ae4fa27df2f116f480175875
sabela <sabela.ramos@inf.ethz.ch>
2016-04-19 18:38:44 +0200

    omp barrier with Roni's parse cores script

--- bench/omp_barrier/compila.sh
+++ bench/omp_barrier/compila.sh
@@ -1,2 +1,3 @@
-gcc -O0 -std=c99 -fopenmp ./omp_bar_bench.c
 
+gcc -O0 -std=c99 -fopenmp ./omp_bar_bench.c -o omp_bar_bench
+gcc -std=c99 parse_cores.c -lnuma -lm -o parse_cores
--- bench/omp_barrier/run.sh
+++ bench/omp_barrier/run.sh
@@ -1,9 +1,18 @@
-export OMP_NUM_THREADS=$1
+
+
+FILL="0,2,4" #fill with list of cores given by parse_cores
+RR="1,2,3" 
+
+MAXTH=$1
 export OMP_PROC_BIND=true
-export GOMP_CPU_AFFINITY="?"
 
-
-./a.out > results.dat
-
+for ((i=2; i<=$MAXTH; i+=1))
+do
+	export OMP_NUM_THREADS=$1
+	export GOMP_CPU_AFFINITY=$FILL
+	./omp_var_bench > omp_fill_$i.dat
+	export GOMP_CPU_AFFINITY=$RR
+	./omp_var_bench > omp_rr_$i.dat
+done
 
 
