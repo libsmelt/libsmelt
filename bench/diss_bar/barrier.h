171b7877fb873a84741e420c7f1d06a09361b8ec
Roni Häcki <roni.haecki@inf.ethz.ch>
2016-04-28 09:58:10 +0200

    T195 - fixing the fix of the dissemination barrier ...

--- bench/diss_bar/barrier.h
+++ bench/diss_bar/barrier.h
@@ -9,7 +9,7 @@
 #define BARRIER_H_
 
 #include "sharedmemory.h"
-
+#include <limits.h>
 
 #if (defined DISSEMINATION_2)
 
@@ -23,11 +23,15 @@ void barrier(int nthreads, sharedMemory_t * shm, int m, int rounds, int my_id,
 	unsigned int actualFlag;
 	int r,i,peer,step;
 
-
+    if(*tag < INT_MAX) {
+        (*tag)+=rounds;
+    } else {
+        (*tag) = 2;
+    }
+    
 	notification_t * notification =  &(shm->notification[index_tag]);
-	notificationLine_t * notificationLines = shm->notification[index_tag].notifications[my_id].notificationLines;
-	//	printf("STARTING BARRIER: %d, m = %d, rounds = %d, op_tag = %d\n", my_id, m, rounds,op_tag);fflush(stdout);
-//	while(!__sync_bool_compare_and_swap(&(notificationLine->flag),0,op_tag));//the while is not necessary, is my line and I can't be in two barriers, they're blocking
+	notificationLine_t * notificationLines = 
+                shm->notification[index_tag].notifications[my_id].notificationLines;
 	actualFlag=op_tag;
 	step = 1;
 	for (r =0; r<rounds; r++){
@@ -37,14 +41,12 @@ void barrier(int nthreads, sharedMemory_t * shm, int m, int rounds, int my_id,
 		for(i=1; i<=m; i++){
 			peer = (my_id - i*step);
 			while(peer<0) peer = nthreads+peer;
-			//printf("my_id = %d, round = %d, peer = %d\n",my_id,r,peer);fflush(stdout);
+
 			while(notification->notifications[peer].notificationLines[r].flag<actualFlag);
-			//printf("my_id = %d, round = %d, peer = %d, its notification line is %d\n",my_id,r,peer,notification->notificationLines[peer].notification);fflush(stdout);
 		}
+
 		step = step * (m+1);
 		actualFlag++;
-		//while(!__sync_bool_compare_and_swap(&(notificationLine->notification),m,0));
-		 //printf("my_id = %d, round = %d FINISHED\n", my_id,r);fflush(stdout);
 	}
 }
 
@@ -62,26 +64,28 @@ void barrier(int nthreads, sharedMemory_t * shm, int m, int rounds, int my_id,
 	unsigned int actualFlag;
 	int r,i,peer,step;
 
+    if(*tag < INT_MAX) {
+        (*tag)+=rounds;
+    } else {
+        (*tag) = 2;
+    }
 
 	notification_t * notification =  &(shm->notification[index_tag]);
-	notificationLine_t * notificationLines = shm->notification[index_tag].notifications[my_id].notificationLines;
-	//	printf("STARTING BARRIER: %d, m = %d, rounds = %d, op_tag = %d\n", my_id, m, rounds,op_tag);fflush(stdout);
-//	while(!__sync_bool_compare_and_swap(&(notificationLine->flag),0,op_tag));//the while is not necessary, is my line and I can't be in two barriers, they're blocking
+	notificationLine_t * notificationLines = 
+            shm->notification[index_tag].notifications[my_id].notificationLines;
 	actualFlag=op_tag;
 	step = 1;
 	for (r =0; r<rounds; r++){
 		notificationLines[0].flag=actualFlag;; //Im the only one writing
 		for(i=1; i<=m; i++){
 			peer = (my_id - i*step);
+
 			while(peer<0) peer = nthreads+peer;
-			//printf("my_id = %d, round = %d, peer = %d\n",my_id,r,peer);fflush(stdout);
+
 			while(notification->notifications[peer].notificationLines[0].flag<actualFlag);
-			//printf("my_id = %d, round = %d, peer = %d, its notification line is %d\n",my_id,r,peer,notification->notificationLines[peer].notification);fflush(stdout);
 		}
 		step = step * (m+1);
 		actualFlag++;
-		//while(!__sync_bool_compare_and_swap(&(notificationLine->notification),m,0));
-		 //printf("my_id = %d, round = %d FINISHED\n", my_id,r);fflush(stdout);
 	}
 }
 
