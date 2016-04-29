955cd4c53e59d57677da0930878eda91f6163b83
Roni Häcki <roni.haecki@inf.ethz.ch>
2016-04-29 09:54:51 +0200

    T195 small change in MPI collective bench run script

--- bench/mpibench/run_collectives.sh
+++ bench/mpibench/run_collectives.sh
@@ -9,6 +9,10 @@ CPUFILE=/proc/cpuinfo
 
 NUMLOG=$(lscpu | grep "^CPU(s)" | tr -d '[[:space:]]' | cut -d ":" -f 2)
 
+NUMPHY=`grep "physical id" $CPUFILE | sort -u | wc -l`
+NUMLOG=`grep "processor" $CPUFILE | wc -l`
+
+
 if [ $NUMLOG > $NUMPHY ]
 then
     echo "Hyperthreads enabled"
@@ -18,16 +22,19 @@ else
     ./parse_cores
 fi
 
-/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n $NUMLOG -rf rank_files/rfile_fill_$NUMLOG -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self broadcast 1
 
-/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n $NUMLOG -rf rank_files/rfile_fill_$NUMLOG -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self broadcast 
 
-/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n $NUMLOG -rf rank_files/rfile_fill_$NUMLOG -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self barrier 1
 
-/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n $NUMLOG -rf rank_files/rfile_fill_$NUMLOG -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self barrier
+/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n 32 -rf rank_files/rfile_fill_32 -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self broadcast 1
 
-/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n $NUMLOG -rf rank_files/rfile_fill_$NUMLOG -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self reduction 1
+/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n 32 -rf rank_files/rfile_fill_32 -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self broadcast 
 
-/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n $NUMLOG -rf rank_files/rfile_fill_$NUMLOG -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self reduction
+/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n 32 -rf rank_files/rfile_fill_32 -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self barrier 1
+
+/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n 32 -rf rank_files/rfile_fill_32 -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self barrier
+
+/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n 32 -rf rank_files/rfile_fill_32 -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self reduction 1
+
+/home/haeckir/openmpi-1.10.2/bin/mpirun -H localhost -n 32 -rf rank_files/rfile_fill_32 -mca rmaps_rank_file_physical 1 -mca --bind-to core -mca blt sm,self reduction
 
 rm -r rank_files
