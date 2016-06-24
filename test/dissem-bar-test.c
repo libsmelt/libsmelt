7378368b45e6f2f338c0acd27592bfe20de14036
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-06-24 10:47:05 +0200

    test: fixed error handling in dissem-bar

--- test/dissem-bar-test.c
+++ test/dissem-bar-test.c
@@ -20,19 +20,17 @@
 #include <pthread.h>
 #include <numa.h>
 #include <sched.h>
+#include <assert.h>
 #include <smlt.h>
 #include <smlt_node.h>
 #include <smlt_barrier.h>
 #include <platforms/measurement_framework.h>
-#include <platforms/linux.h>
 
 #define NUM_RUNS_TEST 10
 #define NUM_RUNS_BENCH 10000
 #define NUM_VALUES 1000
 
 struct smlt_dissem_barrier* bar;
-
-
 
 __thread struct sk_measurement m;
 __thread cycles_t buf[NUM_VALUES];
@@ -45,7 +43,7 @@ void* worker_test(void* arg)
     CPU_ZERO(&cpu_mask);
     CPU_SET(id, &cpu_mask);
     //uintptr_t r[8];
-    sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask);    
+    sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask);
 
     for (uint64_t i = 0; i < NUM_RUNS_TEST; i++) {
         if (smlt_node_get_id() == 0) {
@@ -67,7 +65,7 @@ void* worker_bench(void* arg)
     CPU_ZERO(&cpu_mask);
     CPU_SET(id, &cpu_mask);
     //uintptr_t r[8];
-    sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask);    
+    sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask);
 
     sk_m_init(&m, NUM_VALUES, "barrier", buf);
 
@@ -88,25 +86,24 @@ void* worker_bench(void* arg)
 int main(int argc, char ** argv)
 {
     errval_t err;
-    
+
     err = smlt_init(sysconf(_SC_NPROCESSORS_ONLN), true);
     if (smlt_err_is_fail(err)) {
         printf("SMLT init failed \n");
-    }        
+    }
 
     uint32_t cores[NUM_THREADS] = {0,1,2,3};
     err = smlt_dissem_barrier_init(cores, NUM_THREADS, &bar);
     if (smlt_err_is_fail(err)) {
         printf("SMLT init failed \n");
-    }        
-
+    }
 
     struct smlt_node* node;
     for (uint64_t i=0; i<NUM_THREADS; i++) {
        node = smlt_get_node_by_id(cores[i]);
        err = smlt_node_start(node, worker_test, (void*)(uint64_t) cores[i]);
        if (smlt_err_is_fail(err)) {
-           // XXX
+           assert (!"Smelt error");
        }
     }
 
@@ -114,7 +111,7 @@ int main(int argc, char ** argv)
        node = smlt_get_node_by_id(cores[i]);
        err = smlt_node_join(node);
        if (smlt_err_is_fail(err)) {
-           // XXX
+           assert (!"Smelt error");
        }
     }
 
@@ -123,7 +120,7 @@ int main(int argc, char ** argv)
        node = smlt_get_node_by_id(cores[i]);
        err = smlt_node_start(node, worker_bench, (void*)(uint64_t) cores[i]);
        if (smlt_err_is_fail(err)) {
-           // XXX
+           assert (!"Smelt error");
        }
     }
 
@@ -131,8 +128,9 @@ int main(int argc, char ** argv)
        node = smlt_get_node_by_id(cores[i]);
        err = smlt_node_join(node);
        if (smlt_err_is_fail(err)) {
-           // XXX
+           assert (!"Smelt error");
        }
     }
-}
 
+    return 0;
+}
