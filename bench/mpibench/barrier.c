5508da386983a4637a99ce28052405c18f2b7378
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-08-10 14:22:54 +0200

    cleanup: added headers

--- bench/mpibench/barrier.c
+++ bench/mpibench/barrier.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2013-2016, ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
+ */
 #include <mpi.h>
 #include <time.h>
 #include <math.h>
