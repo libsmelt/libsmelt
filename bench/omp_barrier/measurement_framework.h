f50df23f6b2df27acf687421b12d4202216834a1
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-08-10 13:40:41 +0200

    cleanup: fixed existing headers

--- bench/omp_barrier/measurement_framework.h
+++ bench/omp_barrier/measurement_framework.h
@@ -4,12 +4,12 @@
  */
 
 /*
- * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, ETH Zurich.
+ * Copyright (c) 2013-2016, ETH Zurich.
  * All rights reserved.
  *
  * This file is distributed under the terms in the attached LICENSE file.
  * If you do not find this file, copies can be found by writing to:
- * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+ * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
  */
 
 #ifndef MEASUREMENT_FRAMEWORK_H
